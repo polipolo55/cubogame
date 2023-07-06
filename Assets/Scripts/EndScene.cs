86b6e2154215921922cc3b9fd70212fc3d51c7d7
pol <54399660+polipolo55@users.noreply.github.com>
2023-07-06 20:12:12 +0200

    Revert "Merge branch 'level3_deco' into development"
    This reverts commit 9b2908b8bc84570d07a3f0fd4cfd6d1c83cf8bf0, reversing
    changes made to e5dad085673689418b36b3accf762bb1111381e8.

--- Assets/Scripts/EndScene.cs
+++ Assets/Scripts/EndScene.cs
@@ -1,38 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System;
 
 public class EndScene : MonoBehaviour
 {
     public float eventTimer = 0;
-
     public string levelName;
-
-
-    public Canvas canvas;
-    public GameObject textPrefab;
-    public GameObject leaderBoard;
-    public TextMeshProUGUI maxTimeText;
-    public Vector2 startingPosition;
-    public float lineHeight = 30f;
-
-    private List<float> floatList;
-    private bool textDisplayed = false;
-    private float sum;
-
-
+    // Start is called before the first frame update
 
     void Start()
     {
-        sum = GameManager.Instance.totalTime();
-        floatList = GameManager.Instance.getTimesList();
+        float sum = GameManager.Instance.totalTime();
         Debug.Log(sum);
-        leaderBoard.SetActive(false);
     }
 
+    // Update is called once per frame
     void Update()
     {
         eventTimer += Time.deltaTime;
@@ -43,38 +26,10 @@ public class EndScene : MonoBehaviour
         {
             GameManager.Instance.player.OnJumpInput();
         }
-        if (eventTimer > 0.5f) GameManager.Instance.player.moveInput.x = 1;
+        if(eventTimer > 0.5f) GameManager.Instance.player.moveInput.x = 1;
 
-        if (eventTimer > 2f && !textDisplayed) displayLeaderboard();
-
-        if (eventTimer > 10f) SceneManager.LoadScene(levelName);
+        if(eventTimer > 15f) SceneManager.LoadScene(levelName);
 
 
-    }
-
-    void displayLeaderboard()
-    {
-        textDisplayed = true;
-        leaderBoard.SetActive(true);
-        TimeSpan sumTime = TimeSpan.FromSeconds(sum);
-        string sumText = sumTime.Minutes.ToString("00") + ":" + sumTime.Seconds.ToString("00") + "." + sumTime.Milliseconds.ToString("00");
-        maxTimeText.text = string.Format("{0}", sumText);
-
-        for (int i = 0; i < floatList.Count; i++)
-        {
-            Vector2 position = startingPosition + new Vector2(0f, -lineHeight * i);
-
-            GameObject textObject = Instantiate(textPrefab, Vector3.zero, Quaternion.identity, canvas.transform);
-
-            textObject.GetComponent<RectTransform>().anchoredPosition = position;
-
-            TextMeshProUGUI textMeshPro = textObject.GetComponent<TextMeshProUGUI>();
-
-            TimeSpan time = TimeSpan.FromSeconds(floatList[i]);
-
-            string text = time.Minutes.ToString("00") + ":" + time.Seconds.ToString("00") + "." + time.Milliseconds.ToString("00");
-
-            textMeshPro.text = string.Format("{0}.- {1}", i, text);
-        }
     }
 }
